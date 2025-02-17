441192162e536548d08593a400c6cd1979c92463
marko udovcic <markoudovcic53@gmail.com>
2025-02-17 21:47:34 +0100

    feat: add comment form(textarea) with add/cancel/delete buttons

---
+++ comments/src/components/CommentForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function CommentForm({
+  handleSubmit,
+  submitLabel,
+  hasCancelButton = false,
+  initialText = "",
+  handleCancel,
+}) {
+  const [text, setText] = useState(initialText);
+  const isTextareaDisabled = text.length === 0;
+  const onSubmit = (event) => {
+    event.preventDefault();
+    handleSubmit(text);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <textarea
+        className="comment-form-textarea"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <button className="comment-form-button" disabled={isTextareaDisabled}>
+        {submitLabel}
+      </button>
+      {hasCancelButton && (
+        <button
+          type="button"
+          className="comment-form-button comment-form-cancel-button"
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
+      )}
+    </form>
+  );
+}
+
+export default CommentForm;
